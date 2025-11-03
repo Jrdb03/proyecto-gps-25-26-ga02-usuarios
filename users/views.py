3ab56ef895d6b70f15a34097a2386e195b38f4b1
jbarcogi <jbarcogi@alumnos.unex.es>
2025-11-04 00:08:29 +0100

    feat: Implementado GA02-213

--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegisterRequestSerializer
+
+
+@api_view(['POST'])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = RegisterRequestSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            # Respuesta según el .yaml
+            return Response(
+                {"user_id": user.user_id},
+                status=status.HTTP_201_CREATED
+            )
+
+        # Si hay errores de validación
+        return Response(
+            {
+                "code": "VALIDATION_ERROR",
+                "message": "Error de validación",
+                "details": serializer.errors
+            },
+            status=status.HTTP_422_UNPROCESSABLE_ENTITY
+        )
