aef6cb74680fd861c63c0ee542089477a9178800
jbarcogi <jbarcogi@alumnos.unex.es>
2025-11-11 10:41:11 +0100

    feat: Implementado GA02-270

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from .serializers import RegisterRequestSerializer, LoginRequestSerializer
 
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 
 
 @api_view(['POST'])
@@ -60,3 +61,31 @@ def login_user(request):
             },
             status=status.HTTP_422_UNPROCESSABLE_ENTITY
         )
+
+
+@api_view(['POST'])
+def refresh_token(request):
+    """
+    Endpoint para refrescar tokens JWT
+    """
+    refresh_token = request.data.get('refresh_token')
+
+    # Validar que el refresh token está presente
+    if not refresh_token:
+        return Response(
+            {
+                "code": "VALIDATION_ERROR",
+                "message": "Refresh token es requerido",
+                "details": {"refresh_token": ["Este campo es requerido."]}
+            },
+            status=status.HTTP_422_UNPROCESSABLE_ENTITY
+        )
+
+    # Validar y refrescar el token
+    refresh = RefreshToken(refresh_token)
+    new_tokens = {
+        'access_token': str(refresh.access_token),
+        'refresh_token': str(refresh)
+    }
+
+    return Response(new_tokens, status=status.HTTP_200_OK)
