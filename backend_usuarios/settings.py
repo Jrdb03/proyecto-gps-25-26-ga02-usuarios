f5b4a3b061a3429f7c5f3ef3f344b08867752bff
Jrdb03 <jregaladcv@alumnos.unex.es>
2025-12-06 14:18:09 +0100

    Fix: Solucionada vulnerabilidad de seguridad en SECRET_KEY (settings.py)

--- backend_usuarios/settings.py
+++ backend_usuarios/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d602df)f+c0zvo^yd+b9!ekzk5u+^i23%(akf-0l0!d3$476j^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'k^7z+3@1$q9(m-clave-temporal-dev-mode-8&2!p0_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
